99ad5b6cd5194582c9472a6957f72377721c8f8a
Rashid Shamloo <rashidshamloo@gmail.com>
2023-12-13 19:37:31 +0330

    finished up the home page and added preview image

--- src/components/AppInfo.tsx
+++ src/components/AppInfo.tsx
@@ -1,9 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+
 import { Button } from "./ui/button";
 
 const AppInfo = () => {
   return (
-    <div className="space-y-8 text-center">
-      <h2 className="text-5xl font-bold">Welcome to Food Log!</h2>
+    <div className="flex flex-col items-center justify-center gap-8 text-center">
+      <h2 className="text-[clamp(28px,20px_+_2vw,48px)] font-bold">
+        Welcome to Food Log!
+      </h2>
 
       <p>
         Are you looking to keep track of your daily calorie intake and monitor
@@ -12,7 +17,23 @@ const AppInfo = () => {
         providing you with the tools you need to track your daily calories,
         proteins, carbohydrates, and fats.
       </p>
-
+      <Image
+        src="/preview.png"
+        alt="preview"
+        width="1200"
+        height="800"
+        sizes="(min-width: 768px) 800px, 80vw"
+        className="md:max-w-[70%]"
+        priority
+      />
+      <div className="mt-8">
+        <Button className="mr-8 text-lg" asChild>
+          <Link href="/signup">Sign up and start using now!</Link>
+        </Button>
+        <Button variant="secondary" className="text-lg" asChild>
+          <Link href="/signin">Sign in</Link>
+        </Button>
+      </div>
       <p>
         With Food Log, you can easily log your meals and snacks, and our app
         will automatically calculate the nutritional information for you. Our
@@ -20,13 +41,10 @@ const AppInfo = () => {
         Whether you’re at home or on the go, you can easily log your meals and
         track your progress with Food Log.
       </p>
-
       <p>
         So what are you waiting for? Sign up for Food Log today and start
         achieving your health goals!
       </p>
-
-      <Button className="text-xl">Sign up</Button>
     </div>
   );
 };
