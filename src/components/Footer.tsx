e4e8f1f625d4dac5018336bb202d9dac21ab253b
Rashid Shamloo <rashidshamloo@gmail.com>
2023-12-14 23:06:34 +0330

    changed the footer tooltip to popover so it can be opened on mobile as well

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,48 +1,41 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import authorLinks from "@/data/authorLinks.json";
 
 import { Button } from "./ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 const Footer = () => {
   return (
     <footer className="flex h-8 items-center justify-center text-xs">
-      <div className="[&_img]:hover:animate-heart-bounce flex items-center justify-center gap-1">
+      <div className="flex items-center justify-center gap-1 [&_img]:hover:animate-heart-bounce">
         <span>Made with</span>
         <Image src="/heart.png" alt="heart" width="16" height="16" />
         <span>
           by{" "}
-          <TooltipProvider delayDuration={0}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Button
-                  variant="ghost"
-                  className="h-auto rounded-none p-0 text-xs"
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant="ghost"
+                className="h-auto rounded-none p-0 text-xs"
+              >
+                Rashid Shamloo
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="flex w-auto flex-col gap-1 p-2">
+              {authorLinks.map((link, i) => (
+                <Link
+                  key={i}
+                  href={link.url}
+                  target="_blank"
+                  className="header-link text-sm"
                 >
-                  Rashid Shamloo
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent className="flex flex-col gap-1">
-                {authorLinks.map((link, i) => (
-                  <Link
-                    key={i}
-                    href={link.url}
-                    target="_blank"
-                    className="header-link"
-                  >
-                    {link.title}
-                  </Link>
-                ))}
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
+                  {link.title}
+                </Link>
+              ))}
+            </PopoverContent>
+          </Popover>
         </span>
       </div>
     </footer>
